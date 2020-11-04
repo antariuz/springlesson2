2a3afb6f0f5a7887de8392e8ee232e2f9bb50865
Tymur Foshch <antariuz@gmail.com>
2020-11-04 22:37:57 +0300

    Initial commit

---
+++ src/main/java/my/lesson/springlesson2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package my.lesson.springlesson2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Springlesson2Application.class);
+	}
+
+}
---
+++ src/test/java/my/lesson/springlesson2/Springlesson2ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.lesson.springlesson2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlesson2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
